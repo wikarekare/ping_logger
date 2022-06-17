73d284be502e8fac71abb055b10b9691a8c4740d
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-17 14:04:09 +1200

    copied from running scripts

---
+++ www/js/last_seen_global_state.js
@@ -0,0 +1,48 @@
+var wikk_last_seen_global_state = ( function() {
+  var registered_local_completion = null;
+  var state_record = null;
+
+  function last_seen_error(jqXHR, textStatus, errorMessage) {   //Called on failure
+  }
+
+  function last_seen_completion(data) {   //Called when everything completed, including callback.
+    if(registered_local_completion != null) {
+      registered_local_completion(state_record);
+    }
+  }
+
+  function last_seen_callback(data) {   //Called when we get a response.
+    if(data != null && data.result != null) {
+      state_record = data.result;
+    } else {
+      state_record = null;
+    }
+  }
+
+  function state(local_completion, delay) {
+    if(delay == null) delay = 0;
+    registered_local_completion = local_completion;
+
+    var args = {
+      "method": "LastSeen.global_state",
+      "kwparams": {
+        "select_on": { },  //every active line
+        "orderby": null,
+        "set": null,              //blank, then no fields to update in a GET
+        "result": ['hostname','colour']
+      },
+      "version": 1.1
+    }
+
+    url = "/ruby/rpc.rbx"
+    wikk_ajax.delayed_ajax_post_call(url, args, last_seen_callback, last_seen_error, last_seen_completion, 'json', true, delay);
+    return false;
+  }
+
+  //return a hash of key: function pairs, with the key being the same name as the function.
+  //Hence call with wikk_last_seen.function_name()
+  return {
+    state: state,
+    state_record: state_record
+  };
+})();
---
+++ www/js/ping_graph.js
@@ -0,0 +1,50 @@
+var wikk_ping_graph = ( function() {
+  var registered_local_completion = null;
+  var result = null;
+
+  function ping_data_error_callback(jqXHR, textStatus, errorMessage)
+  {   var result_div = document.getElementById('result_div');
+      last_result = null;
+      result_div.innerHTML='Error:' + jqXHR.status.toString() + '<br>\n' + errorMessage;
+  }
+
+  function ping_data_completion(data) {   //Called when everything completed, including callback.
+    if(registered_local_completion != null) {
+      registered_local_completion(result);
+    }
+  }
+
+  function ping_data_callback(data) { //should we have extra args ,status,xhr
+    result = data.result;
+  }
+
+  function graph_data(form_id, local_completion=null) {
+    registered_local_completion = local_completion;
+
+    var the_form = document.getElementById(form_id);
+    var args = {
+      "method": "Pings.read",
+      "kwparams": {
+        "select_on": { "hostname":   the_form.host.value,
+                       "start_time": the_form.start_datetime.value,
+                       "end_time":   the_form.end_datetime.value
+                      },
+        "orderby": null,
+        "set": null,
+        "result": []
+      },
+      "version": 1.1
+    }
+
+    url = "/ruby/rpc.rbx"
+    wikk_ajax.delayed_ajax_post_call(url, args, ping_data_callback, ping_data_error_callback, ping_data_completion, 'json', true, 0);
+
+    return false;
+  }
+
+  //return a hash of key: function pairs, with the key being the same name as the function.
+  //Hence call with wikk_ping.function_name()
+  return {
+    graph_data: graph_data
+  };
+})();
