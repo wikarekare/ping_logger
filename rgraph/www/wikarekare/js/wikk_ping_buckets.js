033a73b8f8e314585493ab2189099d2f5a28d526
robertburrowes <r.burrowes@auckland.ac.nz>
2022-06-27 17:22:04 +1200

    Script from Rgraph test version

---
+++ rgraph/www/wikarekare/js/wikk_ping_buckets.js
@@ -0,0 +1,53 @@
+var wikk_ping_buckets = ( function() {
+  //MIT License Wikarekare.org rob@wikarekare.org
+
+  var registered_local_completion = null;
+  var result = null;
+
+  function ping_data_error_callback(jqXHR, textStatus, errorMessage)
+  {   var result_div = document.getElementById('result_div');
+      last_result = null;
+      result_div.innerHTML='Error:' + jqXHR.status.toString() + '<br>\n' + errorMessage;
+  }
+
+  function ping_data_completion(data) {   //Called when everything completed, including callback.
+    if(registered_local_completion != null) {
+      registered_local_completion(result);
+    }
+  }
+
+  function ping_data_callback(data) { //should we have extra args ,status,xhr
+    result = data.result;
+  }
+
+  function ping_buckets(the_form, local_completion=null) {
+    registered_local_completion = local_completion;
+
+    var args = {
+      "method": "Pings.buckets",
+      "kwparams": {
+        "select_on": { "hostname":   the_form.host.value,
+                       "start_time": the_form.start_datetime.value,
+                       "end_time":   the_form.end_datetime.value,
+                       "tz":         wikk_ping_form.tz(),
+                       "nbuckets":   20
+                      },
+        "orderby": null,
+        "set": null,
+        "result": []
+      },
+      "version": 1.1
+    }
+
+    url = "/cgi/rpc.rbx"
+    wikk_ajax.delayed_ajax_post_call(url, args, ping_data_callback, ping_data_error_callback, ping_data_completion, 'json', true, 0);
+
+    return false;
+  }
+
+  //return a hash of key: function pairs, with the key being the same name as the function.
+  //Hence call with wikk_ping.function_name()
+  return {
+    ping_buckets: ping_buckets
+  };
+})();
