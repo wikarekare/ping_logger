c1d5e97d579c9fe50b4c91d9f0298014ff263eea
robertburrowes <r.burrowes@auckland.ac.nz>
2023-02-19 08:31:12 +1300

    Changed to using threads, as timeouts to offline switches meant not finishing withing a minute

--- sbin/monitor/switch_port_check.rb
+++ sbin/monitor/switch_port_check.rb
@@ -4,24 +4,17 @@
 require 'snmp'
 require 'time'
 require 'wikk_configuration'
+
 RLIB = '/wikk/rlib' unless defined? RLIB
 require_relative "#{RLIB}/wikk_conf.rb"
 require_relative "#{RLIB}/monitor/lastseen_sql.rb"
 require_relative "#{RLIB}/utility/snmp_override.rb"
 
-port_operation_status = [ 'IF-MIB::ifOperStatus' ]
-port_type = [ 'IF-MIB::ifType' ]
+def check_switch(host_record:)
+  port_operation_status = [ 'IF-MIB::ifOperStatus' ]
+  port_value = []
+  port_type = [ 'IF-MIB::ifType' ]
 
-host = JSON.parse(File.read(SWITCH_CHECK_PORTS))
-
-@mysql_conf = WIKK::Configuration.new(MYSQL_CONF)
-@pingable_hosts = []
-@port_type = []
-
-t = ARGV.length == 1 ? Time.parse(ARGV[0]) : Time.now
-t -= t.sec + t.usec / 1000000.0 # just want this to the minute!
-
-host.each do |host_record|
   h = host_record['hostname']
   community = host_record['community']
   begin
@@ -31,25 +24,41 @@ host.each do |host_record|
         row.each do |vb|
           port = vb.name.to_s.split('.')[-1].to_i
           if ! port.nil?
-            @port_type[port] = (vb.value == 6)
+            port_value[port] = (vb.value == 6)
           end
         end
       end
       manager.walk(port_operation_status) do |row|
         row.each do |vb|
           port = vb.name.to_s.split('.')[-1].to_i
-          next unless port != nil && @port_type[port]
+          next unless port != nil && port_value[port]
 
           if vb.value == 1
-            @pingable_hosts << "#{h}-p#{port}"
+            @pingable_hosts["#{h}-p#{port}"] = "#{h}-p#{port}"
           end
         end
       end
     end
-  rescue StandardError => _e
+  rescue StandardError => e
+    warn "Check_switch(#{h}): #{e}"
     # Ignore errors
   end
 end
 
-# p @pingable_hosts
-Lastseen.record_pings(@mysql_conf, @pingable_hosts, t)
+hosts = JSON.parse(File.read(SWITCH_CHECK_PORTS))
+
+@mysql_conf = WIKK::Configuration.new(MYSQL_CONF)
+@pingable_hosts = {}
+
+t = ARGV.length == 1 ? Time.parse(ARGV[0]) : Time.now
+t -= t.sec + t.usec / 1000000.0 # just want this to the minute!
+
+threads = []
+
+hosts.each do |h|
+  threads << Thread.new(h) { |hr| check_switch(host_record: hr) }
+end
+
+threads.each(& :join)
+
+Lastseen.record_pings(@mysql_conf, @pingable_hosts.keys, t)
